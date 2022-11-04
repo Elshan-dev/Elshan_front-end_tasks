d5fbe9945bf93cae27f8f10ae595e7334675b5f0
Elshanjed <elschenism@gmail.com>
2022-11-04 14:47:21 +0400

    add JS task 1

---
+++ JavaScript/task1/alert.js
@@ -0,0 +1,12 @@
+let width = Number(prompt("Enter width:"))
+let height = Number(prompt("Enter height:"))
+let area =width*height
+alert(area)
+
+let birthdate = Number(prompt("Enter your birth date:"))
+let age =2022-birthdate
+alert(age)
+
+let km = Number(prompt("Enter km:"))
+let mil =0.6214*km
+alert(mil)
