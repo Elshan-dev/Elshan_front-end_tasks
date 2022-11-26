b0667d6b1afcda12104310e24ab73dff80c860db
Elshanjed <elschenism@gmail.com>
2022-11-26 16:46:14 +0400

    add task6

---
+++ JavaScript/task6/app.js
@@ -0,0 +1,59 @@
+// Task 1
+
+const obj1 = {
+    ad: 'ger',
+    soyad: 'man'
+}
+
+const obj2 = {
+    ad: 'murebbe',
+    soyad: 'eldarov'
+}
+
+function deepclone(ob1,ob2){
+for (let key in ob1){
+    ob2[key]=ob1[key]
+}
+}
+
+deepclone(obj1,obj2)
+
+console.log(obj1,obj2)
+
+// Task 2
+
+let obj = {
+    ad: 'ger',
+    soyad: 'man'
+}
+
+function deleteProperty (a,b){
+if (a===obj.soyad){
+    delete b['soyad']
+}
+else if (a===obj.ad){
+    delete b['ad']
+}
+}
+
+deleteProperty(obj.soyad,obj)
+
+console.log(obj.ad,obj.soyad)
+
+// Task 3
+
+const object = {
+    ad: 'pizza' ,
+    dad: 'ela' ,
+    olcu: '25sm',
+}
+
+let say = 0
+function objectCount (){
+for(let key in object){
+    say+=1
+}
+}
+
+objectCount()
+console.log (say)   
