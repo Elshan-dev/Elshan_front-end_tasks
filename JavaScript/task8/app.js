a892957ae4a9527dd10eade8ad81ed8f60053c83
Elshanjed <elschenism@gmail.com>
2022-12-03 15:55:50 +0400

    add task 8

---
+++ JavaScript/task8/app.js
@@ -0,0 +1,47 @@
+// array
+// Task  1 promptla massiv yaratmaq
+
+// let arr1 = []
+// arr1.length = 5
+// for (let i = 0 ; i<arr1.length ; i++) {
+//     arr1[i] = +prompt('Enter numbers:')
+// }
+// console.log(arr1)
+
+// Task 2 multiplyArray
+
+// let arr2 = [3,4,2]
+// function multiplyArray (arr2){
+// let multiplyStart = 1
+// for(let i = 0 ; i < arr2.length ;i++ ) {
+// multiplyStart *= arr2[i]
+// }
+// return multiplyStart
+// }
+// let answer1 = multiplyArray(arr2)
+// console.log(answer1)
+
+// Task 3 multiply
+
+// let arr3 = []
+// function multiply(...arr3){
+//     let multiStart = 1
+//     for (let i = 0 ; i<arr3.length ; i++){
+// multiStart *= arr3[i]
+//     }
+//     return multiStart
+// }
+
+// let answer3 = multiply(4,5,6)
+// console.log(answer3)
+
+// Task 4 remove
+
+// arr4 = []
+// function removeElement(arr4,z){
+//         arr4.splice(arr4.indexOf(z),1)
+//     return arr4
+// }
+
+// let answer4 = removeElement([1,2,3,4,21,421,5],1)
+// console.log(answer4)
