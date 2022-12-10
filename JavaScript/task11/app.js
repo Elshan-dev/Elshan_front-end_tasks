81bf4d53abffe38708f45f772d20e276242426c4
Elshanjed <elschenism@gmail.com>
2022-12-10 21:35:56 +0400

    add task 10

---
+++ JavaScript/task11/app.js
@@ -0,0 +1,17 @@
+// Task circle
+
+let diameterElement = document.getElementById('diameter')
+let buttonElement = document.getElementById('btn')
+let lengthElement = document.getElementById('length')
+let areaElement = document.getElementById('area')
+let circleElement = document.getElementById('circle')
+
+buttonElement.onclick = () => {
+    if (diameterElement.value >=10 && diameterElement.value <=100){
+circleElement.style.width = diameterElement.value +'px'
+circleElement.style.height = diameterElement.value +'px'
+lengthElement.innerHTML = `Length of circle: ${parseInt(diameterElement.value*Math.PI)}`
+areaElement.innerHTML = `Area of circle: ${parseInt(diameterElement.value/2*Math.PI*diameterElement.value/2)}`
+}
+else {alert('You had one job 🤦🏻‍♂️')}
+}
