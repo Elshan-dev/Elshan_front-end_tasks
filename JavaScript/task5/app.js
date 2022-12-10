abece740f1b4d6eb92d1a380789854b359e157e5
Elshanjed <elschenism@gmail.com>
2022-12-10 15:47:08 +0400

    add task 5

---
+++ JavaScript/task5/app.js
@@ -0,0 +1,32 @@
+// Bir parametrli funksiya yazın, parametr olaraq cins(K,Q) və ad qəbul etsin. 
+// Həmin funksiya geriyə funksiya göndərsin. 
+// Geriyə göndərdiyi funksiyada əgər cins qadındırsa qarşısına Xanım, kişidirsə cənab yazılıb, console-a çıxarılsın.
+
+// // Task 1 callback
+
+// function add(num1,num2,callback) {
+// if (callback === console.log) {
+// console.log(num1 + num2)
+// } 
+// else if (callback === alert) {
+// alert(num1 + num2)
+// }
+// }
+// add(9, 10, console.log);
+
+// // Task 2 womn.☕
+
+// function genderReveal (gender) {
+//     if (gender === "M") {
+//       return function (name) {
+//         console.log(`Mr ${name}`)
+//       }
+//     } else if (gender === "W")
+//       return function (name) {
+//         console.log(`Mrs ${name}`)
+//       }
+//   }
+//   const forMan = genderReveal("M")
+//   forMan("Coşqun")
+//   const forWoman = genderReveal("W")
+//   forWoman("Rafael")
