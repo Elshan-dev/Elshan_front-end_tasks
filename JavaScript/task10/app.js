b26e508fc3ee7cb91ccc1defeeef6f968b251531
Elshanjed <elschenism@gmail.com>
2022-12-10 18:38:56 +0400

    add task 10

---
+++ JavaScript/task10/app.js
@@ -0,0 +1,22 @@
+let body = document.querySelector('body')
+let circleOneElement = document.querySelector('.one')
+
+circleOneElement.onclick = () => {
+    body.style.backgroundColor = 'grey'
+}
+
+let circleTwoElement = document.querySelector('.two')
+
+circleTwoElement.onclick = () => {
+    body.style.backgroundColor = 'red'
+}
+let circleThreeElement = document.querySelector('.three')
+
+circleThreeElement.onclick = () => {
+    body.style.backgroundColor = 'blue'
+}
+let circleFourElement = document.querySelector('.four')
+
+circleFourElement.onclick = () => {
+    body.style.backgroundColor = 'yellow'
+}
