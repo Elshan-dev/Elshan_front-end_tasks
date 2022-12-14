643d6fefb26a71607518c6a971917504e160e48d
Elshanjed <elschenism@gmail.com>
2022-12-14 21:47:44 +0400

    add task12

--- JavaScript/task12/app.js
+++ JavaScript/task12/app.js
@@ -50,27 +50,51 @@ const users=[
 ]
 const root=document.getElementById('root')
 
-function createElem() {
-// A: calis forEach-den istifade et
-    for (let i = 0; i <8; i++) {
-        const newEl = document.createElement('div')
-        root.append(newEl)
-        newEl.className = 'newelement'
+// function createElem() {
+// // A: calis forEach-den istifade et
+//     for (let i = 0; i <8; i++) {
+//         const newEl = document.createElement('div')
+//         root.append(newEl)
+//         newEl.className = 'newelement'
 
-        const photo = document.createElement('img')
-        photo.setAttribute('src',`./img/${users[i].img}.jpg`)
-        newEl.appendChild(photo)
-        photo.className = 'photo'
+//         const photo = document.createElement('img')
+//         photo.setAttribute('src',`./img/${users[i].img}.jpg`)
+//         newEl.appendChild(photo)
+//         photo.className = 'photo'
         
-        const name = document.createElement('h1')
-        name.textContent = users[i].username
-        newEl.appendChild(name)
-        name.className = 'name'
+//         const name = document.createElement('h1')
+//         name.textContent = users[i].username
+//         newEl.appendChild(name)
+//         name.className = 'name'
         
-        const job = document.createElement('h3')
-        job.textContent = users[i].job
-        newEl.appendChild(job)
-        job.className = 'job'
-    }
+//         const job = document.createElement('h3')
+//         job.textContent = users[i].job
+//         newEl.appendChild(job)
+//         job.className = 'job'
+//     }
+//   }
+//   createElem()
+
+function createElemV2() {
+    users.forEach(({}, index) => {
+      const newEl = document.createElement("div")
+      root.append(newEl)
+      newEl.className = "newelement"
+  
+      const photo = document.createElement("img");
+      photo.setAttribute("src", `./img/${users[index].img}.jpg`)
+      newEl.appendChild(photo)
+      photo.className = "photo"
+  
+      const user = document.createElement("h1");
+      user.textContent = users[index].username;
+      newEl.appendChild(user)
+      user.className = "name"
+  
+      const job = document.createElement("h3");
+      job.textContent = users[index].job
+      newEl.appendChild(job)
+      job.className = "job"
+    })
   }
-  createElem()
+  createElemV2();
