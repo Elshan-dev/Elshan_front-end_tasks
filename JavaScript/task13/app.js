370f081c93403a15a6ca9d92168bb8beac549a2c
Elshanjed <elschenism@gmail.com>
2022-12-14 23:00:53 +0400

    add task13

---
+++ JavaScript/task13/app.js
@@ -0,0 +1,16 @@
+const rootElem = document.getElementById('root')
+const simsim = document.createElement('h2')
+rootElem.appendChild(simsim)
+
+let message = ''
+window.addEventListener('keydown',(element)=> {
+    if(element.key === 'Backspace'){
+        message = message.slice(0,message.length-1)
+    }
+    else{
+    message+= element.key
+}
+    if(message === 'simsimacil'){
+        simsim.textContent = 'xos geldin menim agam'
+    }
+})
