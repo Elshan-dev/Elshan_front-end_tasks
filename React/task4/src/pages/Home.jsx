19679c42134148fc5f91ae2dedfe09991d8a3414
Elshan <115345919+Elshan-dev@users.noreply.github.com>
2023-02-04 15:56:04 +0400

    Delete React/task4 directory

--- React/task4/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import { PageContainer } from "components/PageContainer";
-
-export function Home({}) {
-  return (
-    <PageContainer>
-      <div>Home</div>
-    </PageContainer>
-  );
-}
--- React/task4/src/utils/index.js
+++
@@ -1,6 +0,0 @@
-export function addGmail(txt){
-    if(!txt.endsWith('@gmail.com')){
-      return txt + "@gmail.com"
-    }
-    return txt
-  }
