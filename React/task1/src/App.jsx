2e06982758fea3ba0e0b9bea8d895bbdc4a9e8e4
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-02-06 10:25:23 +0400

    done ✅

--- React/task1/src/App.jsx
+++ React/task1/src/App.jsx
@@ -1,14 +1,15 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
-import {Main} from "./components/Main"
-
+import { Main } from "./components/Main"
+// A: done ✅
+// A: bir kicik nuans budur ki, file-i format edek. shift+alt+f
 function App() {
   return (
-  <>
-<Header text="BMW" bgColor="blue"/>  
-<Main heading="E46" text="330M" width="50%" height="50%"/>
-<Footer text="You can stick a BMW badge on a dead cat - and people would still buy it." bgColor="red"/>
-  </>
+    <>
+      <Header text="BMW" bgColor="blue" />
+      <Main heading="E46" text="330M" width="50%" height="50%" />
+      <Footer text="You can stick a BMW badge on a dead cat - and people would still buy it." bgColor="red" />
+    </>
   );
 }
 
